bec162e02c6a8cac722493e874f16745f570453f
hendre-21 <henry_liwag@yahoo.com>
2017-05-19 18:00:51 -0700

    created helloworld java

---
+++ src/final1/HelloWorld.java
@@ -0,0 +1,14 @@
+package final1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		for (int  i = 0; i < 26; i++) {
+			System.out.println("my name is Henry");
+		}
+		
+
+	}
+
+}
